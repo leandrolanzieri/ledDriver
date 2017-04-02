9f86f74e1c2b0e2a21562d0d9c079025e75f0e6a
Leandro Lanzieri <lanzierileandro@gmail.com>
2017-04-02 00:11:18 -0300

    Agrego los tests de 2 a 7

--- src/ledDriver.c
+++ src/ledDriver.c
@@ -4,3 +4,19 @@
 void ledDriver_create(uint16_t* address) {
     *address = 0;
 }
+
+void ledDriver_led_on(uint16_t* address, uint8_t led) {
+    
+}
+
+void ledDriver_all_leds_on(uint16_t* address) {
+
+}
+
+void ledDriver_all_leds_off(uint16_t* address) {
+    
+}
+
+uint8_t ledDriver_get_led_state(uint16_t address, uint8_t led) {
+
+}
--- test/test_ledDriver.c
+++ test/test_ledDriver.c
@@ -23,3 +23,35 @@ void test_DriverCreateDebeApagarTodosLosLeds() {
     ledDriver_create(&address);
     TEST_ASSERT_EQUAL_HEX16(0, address);
 }
+
+void test_DriverLedOnDebeEncenderLedSeleccionado() {
+    ledDriver_led_on(&address, 5);
+    TEST_ASSERT_EQUAL_HEX16(0x0020, address);
+}
+
+void test_DiverLedOffDebeApagarLedSeleccionado() {
+    address = 0xFFFF;
+    ledDriver_led_on(&address, 5);
+    TEST_ASSERT_EQUAL_HEX16(0xFFDF, address);
+}
+
+void test_DriverAllLedsOnDebeEncenderTodosLosLeds() {
+    address = 0;
+    ledDriver_all_leds_on(&address);
+    TEST_ASSERT_EQUAL_HEX16(0xFFFF, address);
+}
+
+void test_DriverAllLedsOffDebeApagarTodosLosLeds() {
+    address = 0xFFFF;
+    ledDriver_all_leds_off(&address);
+    TEST_ASSERT_EQUAL_HEX16(0, address);
+}
+
+void test_DriverGetLedStateDebeDarEstadoLed() {
+    address = 1;
+    uint8_t led = ledDriver_get_led_state(address, 1);
+    TEST_ASSERT_EQUAL_HEX16(1, led);
+
+    led = ledDriver_get_led_state(address, 16);
+    TEST_ASSERT_EQUAL_HEX16(0, led);
+}
