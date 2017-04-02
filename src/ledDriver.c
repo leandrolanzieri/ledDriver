6b4eefeb91d9661c1bc9f1b24c510bac21019e8d
Leandro Lanzieri <lanzierileandro@gmail.com>
2017-04-02 00:29:12 -0300

    Implemento las funciones vacias del driver

--- src/ledDriver.c
+++ src/ledDriver.c
@@ -6,17 +6,18 @@ void ledDriver_create(uint16_t* address) {
 }
 
 void ledDriver_led_on(uint16_t* address, uint8_t led) {
-    
+    *address |= (1 << (led - 1)); // Los leds estan numerados de 1 a 16
 }
 
 void ledDriver_all_leds_on(uint16_t* address) {
-
+    *address = 0xFFFF;
 }
 
 void ledDriver_all_leds_off(uint16_t* address) {
-    
+    *address = 0;   
 }
 
 uint8_t ledDriver_get_led_state(uint16_t address, uint8_t led) {
-
+    uint8_t state = address & (1 << (led - 1));
+    return state;
 }
