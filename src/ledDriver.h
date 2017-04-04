ea47afab1045429f512efd9d06e542c6826c2bda
Leandro Lanzieri <lanzierileandro@gmail.com>
2017-04-03 21:35:25 -0300

    FIX: Cambio tipo de dato que retorna la lectura de led y su test

--- src/ledDriver.h
+++ src/ledDriver.h
@@ -6,4 +6,4 @@ void ledDriver_led_on(uint16_t*, uint8_t);
 void ledDriver_led_off(uint16_t*, uint8_t);
 void ledDriver_all_leds_on(uint16_t*);
 void ledDriver_all_leds_off(uint16_t*);
-uint8_t ledDriver_get_led_state(uint16_t, uint8_t);
+int8_t ledDriver_get_led_state(uint16_t, uint8_t);
